025dbb4d440cd310f5e09581921c421a9a4770e4
shishirrahman00 <shishirrahman23@gmail.com>
2025-10-02 14:48:24 +0600

    Fixed Add to cart quantity problem

--- src/reducer/productReducer.js
+++ src/reducer/productReducer.js
@@ -8,10 +8,25 @@ export const productReducer = (state, action) => {
         products: action.payload,
       };
     case ADD_TO_CART:
-      return {
-        ...state,
-        productCart: [...state.productCart, action.payload],
-      };
+      if (
+        state.productCart.find((item) => {
+          return item.id === action.payload.id;
+        })
+      ) {
+        return {
+          ...state,
+          productCart: state.productCart.map((item) => {
+            return item.id === action.payload.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item;
+          }),
+        };
+      } else {
+        return {
+          ...state,
+          productCart: [...state.productCart, action.payload],
+        };
+      }
 
     default:
       return state;
